22080ccd8bb67de1e6e19d0fcf644177e704f3ba
munyuaDeveloper <munyuapeter06@gmail.com>
2020-03-29 22:12:51 +0300

    updated for heroku production bug fix

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<Quexl>'));
+app.use(express.static(__dirname + '/dist/<quexl123>'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<Quexl>/index.html'));
+res.sendFile(path.join(__dirname+'/dist/<quexl123>/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
